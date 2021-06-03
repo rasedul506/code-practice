2b355b22a3f86b553d3eecde057bf391a3d80bed
rasedulhauque <md.rasedulhauque@gmail.com>
2021-06-02 20:05:55 -0700

    Update singlylinkedlist.py

--- singlylinkedlist.py
+++ singlylinkedlist.py
@@ -73,8 +73,32 @@ class SinglyLinkedList:
                 cur = prev
             prev = cur
             cur = cur.next
+    
+    def findLoopNode(self, head):
+        if not head or not head.next: return
 
-     
+        slow = fast = head
+        slow = slow.next
+        fast = fast.next.next
+
+        while(fast and fast.next):
+            if slow == fast:
+                break
+            slow = slow.next
+            fast = fast.next.next
+        
+        if slow != fast:
+            return None
+        
+        slow = head
+
+        while(slow != fast):
+            slow = slow.next
+            fast = fast.next
+        return slow
+        
+
+        
 
 s = SinglyLinkedList()
 print(s.display())
