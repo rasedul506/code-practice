cf1b304049fe67dc4b120d060c8c7e552cbbb118
rasedulhauque <md.rasedulhauque@gmail.com>
2021-06-20 17:15:19 -0700

    Update SubsetII.py

--- Recursion/SubsetII.py
+++ Recursion/SubsetII.py
@@ -1,1 +1,12 @@
-#
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums = sorted(nums)
+        result = [[]]
+        
+        for num in nums:
+            length = len(result)
+            for i in range(length):
+                subset = result[i]+[num]
+                if subset not in result:
+                    result.append(subset)
+        return result
