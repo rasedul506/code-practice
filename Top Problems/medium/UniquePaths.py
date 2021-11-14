62d99486f9c6f4b79155e65846de663122769b34
rasedulhauque <md.rasedulhauque@gmail.com>
2021-11-13 20:51:34 -0800

    Update UniquePaths.py

--- Top Problems/medium/UniquePaths.py
+++ Top Problems/medium/UniquePaths.py
@@ -1,11 +1,10 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        puzzle=[[0 for _ in range(m+1)] for _ in range(n+1)]
-        for j in range(1,n+1):
-            for i in range(1,m+1):
-                if i==1 and j==1:
-                    puzzle[j][i]=1
+        puzzle = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        for i in range(1,m+1):
+            for j in range(1, n+1):
+                if i==j==1:
+                    puzzle[i][j]=1
                 else:
-                    puzzle[j][i]=puzzle[j-1][i]+puzzle[j][i-1]
-                print(puzzle)
-        return puzzle[n][m]
+                    puzzle[i][j]=puzzle[i-1][j]+puzzle[i][j-1]
+        return puzzle[m][n]
